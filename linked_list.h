0906117da69ad30ca4b8a6716b112d381d5c154b
henryz2000 <hzheng3@stuy.edu>
2017-10-17 11:01:54 -0400

    finished linked_list functions

--- linked_list.h
+++ linked_list.h
@@ -4,8 +4,8 @@
 void print_list(struct song_node *);
 struct song_node * insert_front(struct song_node *, char *, char *);
 struct song_node * insert_ordered(struct song_node *, char *, char *);
-struct song_node * find_song(struct song_node *, char *);
+struct song_node * find_song(struct song_node *, char *, char *);
 struct song_node * first_song(struct song_node *, char *);
 struct song_node * random_song(struct song_node *);
-int remove_node(struct song_node *, char *);
+int remove_song(struct song_node *, char *, char *);
 struct song_node * free_list(struct song_node *);
