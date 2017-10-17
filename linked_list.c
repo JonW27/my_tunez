1554c53825855b83ce1c6563cdc76b610d7d41e2
henryz2000 <hzheng3@stuy.edu>
2017-10-17 01:23:15 -0400

    up to remove_node. add in testing code for remove_node. figure out what parameter to remove by

--- linked_list.c
+++ linked_list.c
@@ -1,28 +1,85 @@
-#include "struct_node.h"
+#include "song_node.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 //start of functions
 
-void print_list(struct node *list){
+void print_list(struct song_node *list){
   while (list){
-    printf("%d\n", list->i);
+    printf("%s -- %s\n", list->name, list->artist);
     list = list->next;
   }
 }
 
-struct node * insert_front(struct node *list, int data){
-  struct node *new = malloc(sizeof(struct node));
-  
-  new->i = data;
+struct song_node * insert_front(struct song_node *list, char *name, char *artist){
+  struct song_node *new = (struct song_node *)malloc(sizeof(struct song_node));
+
+  strcpy(new->name, name);//dest, src
+  strcpy(new->artist, artist);
   new->next = list;
   return new;
 }
 
-struct node * free_list(struct node *list){
+struct song_node * insert_ordered(struct song_node *list, char *name, char *artist){
+  struct song_node *new = (struct song_node *)malloc(sizeof(struct song_node));
+
+  strcpy(new->name, name);
+  strcpy(new->artist, artist);
+  while ( strcmp(new->name, list->name) > 0 ){
+    if( strcmp(new->name, list->next->name) < 0 ){
+      new->next = list->next;
+      list->next = new;
+      break;
+    }
+    list = list->next;
+  }
+  return new;
+}
+
+struct song_node * find_song(struct song_node *list, char *name){
   while (list){
-    struct node *nextup = list->next;
-    printf("freed\t%p: %d\n", list, list->i);
+    if (strcmp(list->name, name) == 0){
+      return list;
+    }
+    list = list->next;
+  }
+  return list;
+}
+
+struct song_node * first_song(struct song_node *list, char *artist){
+  while (list){
+    if (strcmp(list->artist, artist) == 0){
+      return list;
+    }
+    list = list->next;
+  }
+  return list;
+}
+
+struct song_node * random_song(struct song_node *list){
+  int num_songs = 0;
+  struct song_node *temp = list;
+  while (temp){
+    num_songs++;
+    temp = temp->next;
+  }
+  srand(time(NULL));
+  int r = rand() % num_songs;
+  while (r != 0){
+    list = list->next;
+    r--;
+  }
+  return list;
+}
+
+int remove_node(struct song_node *list, char *
+
+struct song_node * free_list(struct song_node *list){
+  while (list){
+    struct song_node *nextup = list->next;
+    printf("freed %p: %s -- %s\n", list, list->name, list->artist);
     free(list);
     list = nextup;
   }
--- linked_list.h
+++ linked_list.h
@@ -1,6 +1,11 @@
-#include "struct_node.h"
+#include "song_node.h"
 
 //function headers
-void print_list(struct node *);
-struct node * insert_front(struct node *, int);
-struct node * free_list(struct node *);
+void print_list(struct song_node *);
+struct song_node * insert_front(struct song_node *, char *, char *);
+struct song_node * insert_ordered(struct song_node *, char *, char *);
+struct song_node * find_song(struct song_node *, char *);
+struct song_node * first_song(struct song_node *, char *);
+struct song_node * random_song(struct song_node *);
+int remove_node(struct song_node *, char *);
+struct song_node * free_list(struct song_node *);
